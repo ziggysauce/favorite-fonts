734d7de931f75b1487001deee188c0dcd7d9d551
ziggysauce <dtn3423@gmail.com>
2021-11-23 19:55:38 -0500

    some linter cleanup

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+const React = require('react');
+
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
