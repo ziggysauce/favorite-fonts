efc8b1dd27b970f009d12c3049955f19b692936f
ziggysauce <dtn3423@gmail.com>
2021-12-20 00:08:14 -0500

    lemme try this

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const app = express();
 const router = express.Router();
 const port = SERVER_PORT;
 
-app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
 router.get('/api-test', async (req, res) => {
   res.status(200).json({ data: 'Hello there.' });
@@ -35,13 +35,24 @@ app.use(router);
 
 if (NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'build')));
-  // app.get('/', (req, res) => {
-  //   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  // });
 
   // All other GET requests not handled before will return our React app
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+
+  // Catch 404 and forward to error handler
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
   });
 }
 
