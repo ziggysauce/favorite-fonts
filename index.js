9877c86e35d5c1028b9c16dc16d6438a25df806f
ziggysauce <dtn3423@gmail.com>
2021-12-20 00:39:34 -0500

    fix paths

--- index.js
+++ index.js
@@ -34,11 +34,11 @@ router.get('/api', async (req, res) => {
 app.use(router);
 
 if (NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'build')));
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
 
   // All other GET requests not handled before will return our React app
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
 
   // Catch 404 and forward to error handler
