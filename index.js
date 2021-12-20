c4485faaabc0ec2b34e3de66e85b1f31e46f90bd
ziggysauce <dtn3423@gmail.com>
2021-12-20 01:00:12 -0500

    switch back to trad appget

--- index.js
+++ index.js
@@ -7,18 +7,17 @@ const fetch = require('node-fetch');
 
 const { NODE_ENV, SERVER_PORT, GOOGLE_FONTS_API_KEY } = process.env;
 const app = express();
-const router = express.Router();
 const port = SERVER_PORT;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-router.get('/api-test', async (req, res) => {
+app.get('/api-test', async (req, res) => {
   res.status(200).json({ data: 'Hello there.' });
 });
 
-router.get('/api', async (req, res) => {
+app.get('/api', async (req, res) => {
   const fetchUrl = `https://www.googleapis.com/webfonts/v1/webfonts?key=${GOOGLE_FONTS_API_KEY}&sort=popularity`;
   let data = [];
   try {
@@ -30,8 +29,6 @@ router.get('/api', async (req, res) => {
   }
   res.status(200).json({ data });
 });
-
-app.use(router);
 
 if (NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
